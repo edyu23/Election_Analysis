b63c65b1e39f56bbffe16758fbe1b9ec94bcf9d9
Edwin Yu <edyu23@hotmail.com>
2022-05-01 00:51:45 -0700

    adding Pypoll.py file.

---
+++ Pypoll.py
@@ -0,0 +1,109 @@
+# Add our dependencies
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+# Declare the empty dictionary
+candidate_votes = {}
+
+# Winning CAndidate and WInning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+        # Print the candidate name from each row
+        candidate_name = row[2]
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+        # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+        # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+        # Save the results to our text file.
+        with open(file_to_save, "w") as txt_file:
+            # print the final vote count to the terminal.
+            election_results = (
+                f"\nElection Result\n"
+                f"-------------------------\n"
+                f"Total Votes: {total_votes:,}\n"
+                f"-------------------------\n")
+            print(election_results, end="")
+
+            # Save the final vote count to the next file.
+            txt_file.write(election_results)
+
+    # Determine the percenterage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # 2. Retreive vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # 4. Print the canidate name and percentage of votes.
+        # print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+        # To do: print out each candidate's name, vote count, and percentage of number votes to the terminal
+        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent = vote_percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    # print(winning_candidate_summary)
+
+
+
+
+
+   
+
+
+
+# Using the open() function with the "w" mode we will write data to the file.
+with open(file_to_save, "w") as txt_file:
+
+# Write some data to the file
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write("Arapahoe\n")
+    txt_file.write("Denver\n")
+    txt_file.write("Jefferson\n")
+
+# Close the file
+    txt_file.close()
+
---
+++ Untitled-5.py
@@ -0,0 +1,6 @@
+temperature = int(input("What is the temperature outside? "))
+
+If temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
